6b40090e390b769c0b440a7daea5fdf28063e13e
Stein van Broekhoven <stein@aapjeisbaas.nl>
2022-09-11 16:47:29 +0200

    styling

--- auto-download.sh
+++ auto-download.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Downloading latest wordpress"
-
-# check if /var/www has files
+# download fresh wordpress is /var/www/index.php does not exist.
 if [ ! -f "/var/www/index.php" ]; then
+    echo "Downloading latest wordpress"
     curl -o /tmp/latest.tar.gz  https://wordpress.org/latest.tar.gz
     tar -xf /tmp/latest.tar.gz -C /tmp/
     mv /tmp/wordpress/* /var/www

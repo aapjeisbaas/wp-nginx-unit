6894d71932bde35a6826c8ae21d99f7518b688f6
Stein van Broekhoven <stein@aapjeisbaas.nl>
2023-02-08 12:22:56 +0100

    re depend on start order in entrypoint

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ RUN curl -sSLf \
     install-php-extensions bcmath bz2 calendar core ctype curl date dom exif fileinfo filter ftp gd gettext hash iconv imagick imap intl json libxml mbstring mcrypt mysqli mysqlnd opcache openssl pcre pdo pdo_mysql pdo_sqlite phar posix pspell reflection session simplexml soap sodium spl sqlite3 standard tidy tokenizer xml xmlreader xmlwriter xsl zip zlib && \
     curl -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
     chmod +x /usr/local/bin/wp
-COPY config.json auto-download.sh /docker-entrypoint.d/ 
-COPY docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+COPY config.json /
+COPY auto-download.sh /docker-entrypoint.d/ 
--- auto-download.sh
+++ auto-download.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+curl_put()
+{
+    RET=$(/usr/bin/curl -s -w '%{http_code}' -X PUT --data-binary @$1 --unix-socket /var/run/control.unit.sock http://localhost/$2)
+    RET_BODY=$(echo $RET | /bin/sed '$ s/...$//')
+    RET_STATUS=$(echo $RET | /usr/bin/tail -c 4)
+    if [ "$RET_STATUS" -ne "200" ]; then
+        echo "$0: Error: HTTP response status code is '$RET_STATUS'"
+        echo "$RET_BODY"
+        return 1
+    else
+        echo "$0: OK: HTTP response status code is '$RET_STATUS'"
+        echo "$RET_BODY"
+    fi
+    return 0
+}
+
 # download fresh wordpress is /var/www/index.php does not exist.
 if [ ! -f "/var/www/index.php" ]; then
     echo "Downloading latest wordpress"
@@ -10,3 +26,6 @@ if [ ! -f "/var/www/index.php" ]; then
 else
     echo "Not downloading wordpress, found an index.php file in /var/www/"
 fi
+
+
+curl_put /config.json "config"
